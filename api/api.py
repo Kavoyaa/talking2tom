1db2b8f26a560c8aea60aafcb404364aa4f4056d
kavoyaa <pandey.kavya@outlook.com>
2025-07-13 05:22:51 +0000

    Remove status endpoint

--- api/api.py
+++ api/api.py
@@ -98,56 +98,5 @@ def ai_response():
         return('no')
 
 
-# To determine if Tom lets the user live or not
-@app.route("/status", methods=["POST"])
-def alive_status():
-    prompt_start = """
-Pretend that you are "Tom" from the 2010 mobile game called "My Talking Tom". 
-It has been 10 years, and you've returned. You're angry. You want revenge. You are now...evil. You want revenge for all the toying I did to you, all the bullying, all the torment I had you go through in the name of fun. You want to "kill" me. And now, I have to convince you to not do so. 
-
-The following conversation takes place between you (Tom) and me.: 
-Tom: .... 
-Me: W-who are you? 
-Tom: "WhO aRe YoU"? Is that how irrelevant I was to you? I am Tom. And I'm here to kill you. 
-Me: Tom? Tom from that mobile game? Wait hold on, d-don't kill me. 
-Tom: After all the toying, all the torment you put me through, you now wish for mercy? How pathetic. Why should I NOT kill you? 
-"""
-
-    prompt_middle = ""
-
-    prompt_end = """
-As Tom, you must now decide whether to let me go, or to kill me.
-Return the output in the form of JSON. Return the following JSON if you wish to let me go:
-{ 
-    "status": "survived" 
-}
-
-Return the output in the form of JSON. Return the following JSON if you wish to kill me:
-{ 
-    "status": "killed" 
-}
-"""
-
-    data = request.get_json()
-    print(data)
-    messages = data.get("messages", [])
-
-    # Inserting the message history into the prompt
-    speaker = "Me"
-    for i in messages:
-        prompt_middle += f"{speaker}: {i}\n"
-
-        if speaker == "Me":
-            speaker = "Tom"
-        else:
-            speaker = "Me"
-
-    response = model.generate_content(
-        contents=f"{prompt_start}\n{prompt_middle}\n{prompt_end}"
-    )
-
-    return jsonify(response.text[8:-4])
-
-
 if __name__ == "__main__":
     app.run(debug=True)
